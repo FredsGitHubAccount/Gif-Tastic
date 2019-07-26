3012aae0f100821934b7f5421ab8b25fc9a8bb88
Fred Saba <fred@fred-saba.com>
2019-07-25 20:29:09 -0700

    got animations working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,51 +10,79 @@ const renderButton = () => {
     for (let i = 0; i<sportsList.length; i++){
 
         let newButton = $("<button>");
-        newButton.addClass("gif-generator")
-        newButton.attr("data-name", sportsList[i])
+        newButton.addClass("newgif btn btn-success")
+        newButton.attr("data-name", sportsList[i],)
         newButton.text(sportsList[i])
         $("#button-holder").append(newButton)
+ 
 
 
     }
 }
 
 
-const gifGenerator = () => {
+function gifGenerator(){
     
 let apiKey = "VJbsDpwKy6yKGBcWTMuS5L3HfkyJvA6n"    
 let userChoice = $(this).attr("data-name")
-let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${userChoice}"&limit=10&offset=0&rating=R&lang=en"`
+console.log(userChoice)
+let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${userChoice}"&limit=10&offset=0&rating=&lang=en`
 
 
-
+$(".gif-holder").empty();
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
     console.log(response)
-})
 
+    let results = response.data
+   
+    for(let i = 0; i< results.length; i++){
+    
+    let imgURL = response.data[i].images.fixed_height.url
+
+    console.log(imgURL)
+    
+    let newGifs = $(`<div class="click-new">`)
+    newGifs.append(`<h2> Rating : ${results[i].rating}</h2>`)
+    let image = $("<img>").attr("src", imgURL);
+    newGifs.append(image)
+
+    $(".gif-holder").append(newGifs)
+ 
+        
+    
+    }
+})
 
 
 }
 
-
 $("#add-sport").on("click", function(event){
     
     event.preventDefault();
+    
 
     let newSport = $("#sport-input").val().trim()
 
-    sportsList.push(newSport)
+    if(newSport.length<1){
+        alert("You must type something in!")
+        return;
+    }
 
+    else{
+
+    sportsList.push(newSport)
     renderButton()
+    $("#sport-input").val("")
+    }
     
 })
 
 $(".gif-generator").on("click")
 
-$(document).on("click", ".gif-generator", gifGenerator)
+$(document).on("click", ".newgif", gifGenerator)
 
 renderButton();
 
@@ -71,39 +99,3 @@ renderButton();
 // 2. Allow the user to add their own button using the search feature
 
 // 3. Display gifs when user clicks on the button
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let showList = ["southpark", "family guy", "archer"] 
-
-
-// for (let i = 0; i<showList.length; i++) {
-
-// let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${showList[i]}"&limit=10&offset=0&rating=R&lang=en"`
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response){
-//     console.log(response.data[3].embed_url)
-//     $("#hello").append(`<div class="holy">${response.data[3].embed_url}</div>`)
-    
-
-
-// })
-
-// }
-
-// $(document).on("click",".holy",function(){
-//     alert("hi")
-// })
