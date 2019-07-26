094794ee351f9e4ac1c71ae4bb97d2f5cc43b7aa
Fred Saba <fred@fred-saba.com>
2019-07-25 21:46:10 -0700

    logic completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,101 +1,122 @@
-
-
+// Global Variables
 let sportsList = ["basketball", "tennis", "golf", "hockey"]
 
+// Function to render buttons, can add additional functions with the add-sport function
 
-const renderButton = () => {
+function renderButton (){
 
     $("#button-holder").empty()
 
-    for (let i = 0; i<sportsList.length; i++){
+    for (let i = 0; i < sportsList.length; i++) {
 
         let newButton = $("<button>");
         newButton.addClass("newgif btn btn-success")
-        newButton.attr("data-name", sportsList[i],)
+        newButton.attr("data-name", sportsList[i])
         newButton.text(sportsList[i])
         $("#button-holder").append(newButton)
- 
 
 
     }
 }
 
+// Will append the rating and gif affiliated with the clicked button's value.
+function gifGenerator() {
 
-function gifGenerator(){
-    
-let apiKey = "VJbsDpwKy6yKGBcWTMuS5L3HfkyJvA6n"    
-let userChoice = $(this).attr("data-name")
-console.log(userChoice)
-let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${userChoice}"&limit=10&offset=0&rating=&lang=en`
+    let apiKey = "VJbsDpwKy6yKGBcWTMuS5L3HfkyJvA6n"
+    let userChoice = $(this).attr("data-name")
+    console.log(userChoice)
+    let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q="${userChoice}"&limit=10&offset=0&rating=&lang=en`
 
 
-$(".gif-holder").empty();
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
+    $(".gif-holder").empty();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
 
-    let results = response.data
-   
-    for(let i = 0; i< results.length; i++){
-    
-    let imgURL = response.data[i].images.fixed_height.url
+        let results = response.data
 
-    console.log(imgURL)
-    
-    let newGifs = $(`<div class="click-new">`)
-    newGifs.append(`<h2> Rating : ${results[i].rating}</h2>`)
-    let image = $("<img>").attr("src", imgURL);
-    newGifs.append(image)
+        if (results.length == 0) {
+            alert("You must search a valid category")
+            return;
 
-    $(".gif-holder").append(newGifs)
- 
-        
-    
-    }
-})
+        }
+
+        else {
+            for (let i = 0; i < results.length; i++) {
+
+                let animateURL = response.data[i].images.downsized.url
+                let stillURL = response.data[i].images.downsized_still.url
+
+                console.log(animateURL)
+                console.log(stillURL)
+
+                // Rating Append
+                let newGifs = $(`<div class="col-md-12">`)
+                newGifs.append(`<h2> Rating : ${results[i].rating}</h2>`)
+
+                // Image Append
+                let image = $("<img>").addClass("toggler img-fluid")
+                image.attr("src", animateURL)
+                image.attr("data-still", stillURL)
+                image.attr("data-animate", animateURL)
+                image.attr("data-state", "animate")
+                newGifs.append(image)
+
+
+                $(".gif-holder").append(newGifs)
+
+            }
+        }
+    })
 
 
 }
 
-$("#add-sport").on("click", function(event){
-    
+function gifToggler() {
+
+
+    let state = $(this).attr("data-state");
+    let animate = $(this).attr("data-animate");
+    let still = $(this).attr("data-still");
+
+    if (state == "animate") {
+
+        $(this).attr("src", still)
+        $(this).attr("data-state", "still")
+    }
+
+    else {
+        $(this).attr("src", animate)
+        $(this).attr("data-state", "animate")
+    }
+}
+
+
+$("#add-sport").on("click", function (event) {
+
     event.preventDefault();
-    
+
+
 
     let newSport = $("#sport-input").val().trim()
 
-    if(newSport.length<1){
+    if (newSport.length < 1) {
         alert("You must type something in!")
         return;
     }
 
-    else{
+    else {
 
-    sportsList.push(newSport)
-    renderButton()
-    $("#sport-input").val("")
+        sportsList.push(newSport)
+        renderButton()
+        $("#sport-input").val("")
     }
-    
+
 })
 
-$(".gif-generator").on("click")
-
-$(document).on("click", ".newgif", gifGenerator)
 
 renderButton();
-
-
-
-
-
-
-
-
-
-// 1. Render buttons at the top within my sportsList
-
-// 2. Allow the user to add their own button using the search feature
-
-// 3. Display gifs when user clicks on the button
+$(document).on("click", ".newgif", gifGenerator)
+$(document).on("click", ".toggler", gifToggler)
